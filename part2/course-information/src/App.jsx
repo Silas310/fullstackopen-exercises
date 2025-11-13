1a8a40540fca8a70818511a48f2b3c41ddfd2631
Silas <silascosta310@gmail.com>
2025-11-13 07:01:44 -0300

    refactor: extract Course into a separate module (2.5)

--- part2/course-information/src/App.jsx
+++ part2/course-information/src/App.jsx
@@ -1,39 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </div>
-)
-
-const Part = (props) => {
-  const { name, exercises } = props.part;
-  console.log(name, exercises);
-
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  const {total} = props;
-  return(
-    <p>There is {total} exercises</p>
-  )
-}
-
-const Course = (props) => {
-  const { course } = props;
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts.reduce((acc, current) => acc + current.exercises, 0)}/>
-    </div>
-  )
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
