2b44df83869597eaf70a9e85c907309c10d10a55
Silas <silascosta310@gmail.com>
2025-11-14 02:09:16 -0300

    feat: implement person add functionality (2.6)

---
+++ part2/the-phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    const person = {
+      name: newName
+    };
+    setPersons(persons.concat(person));
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input onChange={handleInputChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p>{person.name}</p>)}
+    </div>
+    
+  )
+}
+
+export default App
