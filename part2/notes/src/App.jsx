11dd85a57fa3b4f9ee4428e26bebf6fedb0d5e66
Silas <silascosta310@gmail.com>
2025-11-17 02:37:59 -0300

    feat: data fetch from JSON server

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Note = ({ note }) => {
   return (
@@ -6,10 +7,21 @@ const Note = ({ note }) => {
   )
 }
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+}, [])
+  console.log('render', notes.length, 'notes')
   
   const addNote = (event) => {
     event.preventDefault()
