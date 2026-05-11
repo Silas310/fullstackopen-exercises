ea3a77bea66470820dbf42b1d36c00e96a506fe8
Silas <silascosta310@gmail.com>
2026-05-11 19:11:39 -0300

    complete unit tests for totalLikes and finalize (4.4)

--- part4/blog-list/backend/src/utils/list_helper.js
+++ part4/blog-list/backend/src/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return 0;
+  }
+ 
+  return blogs.reduce( (total, currentValue) => {
+    return total + currentValue.likes;
+  }, 0);
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
