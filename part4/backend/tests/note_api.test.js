44c1af65cd54381989f5b246739135476b88c768
Silas <silascosta310@gmail.com>
2026-05-22 20:10:14 -0300

    Part-4-b-Initializing the database before tests

--- part4/backend/tests/note_api.test.js
+++ part4/backend/tests/note_api.test.js
@@ -1,18 +1,48 @@
-const { test, after } = require('node:test')
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
 
 const api = supertest(app)
 // the test requires the Express app (server instance)
 // uses supertest (it uses superagent) to make HTTP requests to the server
 // it does not require the server to be running, supertest will handle that
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, initialNotes.length)
+})
+
+
 // its does the same as other tests
 // its checks if the response is 200 and if the content type is application/json
 after(async () => { // after all tests have run, close the mongoose connection
