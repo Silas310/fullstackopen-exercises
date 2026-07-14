4da2c15701d37befd81d158e4d65af1d4099e59a
Silas <silascosta310@gmail.com>
2026-07-14 15:57:42 -0300

    feat: implement toggleable details in BlogList (5.7) ps: (5.6 done)

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -1,13 +1,36 @@
+import { useState } from 'react'
+
+
 function BlogList({ blogs }) {
+  const [detailsVisibleStatus, setDetailsVisibleStatus] = useState(null)
+  
+
+  const changeVisibleDetails = (blogId) => {
+    if (detailsVisibleStatus === blogId) {
+      setDetailsVisibleStatus(null);
+    } else {
+      setDetailsVisibleStatus(blogId);
+    }
+  }
+
   return (
     <div>
       {blogs.map(blog => (
-        <div key={blog.id}>
-          {blog.title} {blog.author}
+        <div key={blog.id} style={{ border: '1px solid blue'  }}>
+          <p style={{display: 'inline-block', marginRight: '10px'}}>{blog.title}</p>
+          <button onClick={ () => changeVisibleDetails(blog.id) }>{detailsVisibleStatus === blog.id ? 'hide' : 'view'}</button>
+          {detailsVisibleStatus === blog.id && ( // accordion mode
+            <div>
+              <p>Author: {blog.author}</p>
+              <p>URL: {blog.url}</p>
+              <p>Likes: {blog.likes}</p>
+            </div>  
+          )}
         </div>
       ))}
     </div>
   )
 }
+
 
 export default BlogList
