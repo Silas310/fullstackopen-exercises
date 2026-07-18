2c029d5200677d395c3f8d8a1560190629d4d1b6
Silas <silascosta310@gmail.com>
2026-07-18 04:47:09 -0300

    Part5-d Writing on the form
    - Wrap username and password inputs in semantic <label> tags
    - Add explicit type='text' attribute to the username input
    - Adjust button selector to match lowercase 'login' string in HTML

--- part2/notes/tests/note_app.spec.js
+++ part2/notes/tests/note_app.spec.js
@@ -1,11 +1,24 @@
 import { test, expect } from '@playwright/test'
 
-
 test('front page can be opened', async ({ page }) => {
-  // go to front page, locate content and check if it is visible
   await page.goto('http://localhost:5173')
 
   const locator = page.getByText('Notes')
   await expect(locator).toBeVisible()
-  await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
+  await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+})
+
+test.describe('Note app', () => {
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'log in' }).click()
+    
+    await page.getByLabel('username').fill('admin')
+    await page.getByLabel('password').fill('12345')
+
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Silas logged in')).toBeVisible()
+  })
 })
